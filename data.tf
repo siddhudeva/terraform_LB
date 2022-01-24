2d860eeea856c87e9be9653f8f92455c48b6ed08
siddhu <siddhudangeti@gmail.com>
2022-01-24 17:12:23 +0530

    new s3 bucket is added

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraformbucket020"
+    bucket = "terraformbucket021"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
